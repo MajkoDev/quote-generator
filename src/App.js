80613bad0724e5b246daa23e4906e6b5d3ec856b
Marian <hellomariandev@gmail.com>
2022-04-24 20:21:16 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react"
+import Quote from "./components/Quote";
+
 function App() {
-  return <div className='App'></div>;
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null,
+  });
+
+  const fetchQuote = async () => {
+    return await fetch("https://animechan.vercel.app/api/random").then(
+      (response) => response.json()
+    );
+  };
+
+  const generate = async () => {
+    setQuote(await fetchQuote());
+  }
+
+  useEffect(async () => {
+    setQuote(await fetchQuote());
+  }, []);
+
+  return (
+    <div className='App'>
+      <Quote quote={quote} />
+      <button onClick={generate}>Generate new quote</button>
+    </div>
+  );
 }
 
 export default App;
